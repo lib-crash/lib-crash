fd14a877633feeacc189b50466060c4e6296905b
ChillerDragon <ChillerDragon@gmail.com>
2019-11-01 17:39:30 +0100

    Add crash_install and try to support root

--- install.sh
+++ install.sh
@@ -35,6 +35,7 @@ function crash_include() {
         exit 1
     fi
 
+    dir="\$(pwd)"
     file="\$1"
     lib_path=/usr/lib/lib-crash
     if [ "\$LIB_CRASH_PATH" != "" ]
@@ -54,9 +55,48 @@ function crash_include() {
         exit 1
     fi
 
+    # get path to file and navigate there
+    # so relative includes still work
+    cd "\$lib_path/"
+    if [[ "\$file" =~ / ]]
+    then
+        cd "\${file%/*}"
+    fi
     . "\$lib_path/\$file"
+    cd "\$dir"
+}
+function crash_install() {
+    if [ "\$#" -lt 1 ]
+    then
+        echo "[lib-crash] install failed: missing argument"
+        return 1
+    fi
+
+    repo="\$1"
+    repo="\${repo%%+(/)}" # strip trailing slash
+    lib="\${repo##*/}"    # get last word after slash
+    lib_path=/usr/lib/lib-crash
+    if [ "\$LIB_CRASH_PATH" != "" ]
+    then
+        lib_path="\$LIB_CRASH_PATH"
+    fi
+
+    if [ ! -d "\$lib_path" ]
+    then
+        echo "[lib-crash] install failed: lib-crash not found '\$lib_path'"
+        return 1
+    fi
+
+    if [ -d "\$lib_path/\$lib" ]
+    then
+        echo "[lib-crash] install failed: lib installed already '\$lib_path/\$lib'"
+        return 1
+    fi
+
+    git clone --recursive "\$repo" "\$lib_path/\$lib"
 }
 export -f crash_include
+export -f crash_install
 export LIB_CRASH_VERSION="$current_version"
 # lib-crash end
 EOF
@@ -64,12 +104,15 @@ EOF
 arg_method=${1:-local}
 arg_path=${2:-/usr/lib/lib-crash}
 bash_file=""
-if [ -f /etc/bash.bashrc ] # debian
+bash_file_root=""
+if [ -f /etc/bash.bashrc ] && [ -f /root/.bashrc ] # debian
 then
     bash_file=/etc/bash.bashrc
-elif [ -f /etc/bashrc ] # redhat
+    bash_file_root=/root/.bashrc
+elif [ -f /etc/bashrc ] && [ -f /root/.bashrc ] # redhat
 then
     bash_file=/etc/bashrc
+    bash_file_root=/root/.bashrc
 else
     log "install failed: unsupported os"
     exit 1
@@ -93,6 +136,8 @@ function pre_install() {
     # insert at start of file before any return
     echo "$crash_include" | cat - "$bash_file" > /tmp/lib-crash-bashrc || exit 1
     mv /tmp/lib-crash-bashrc "$bash_file" || exit 1
+    echo "$crash_include" | cat - "$bash_file_root" > /tmp/lib-crash-bashrc-root || exit 1
+    mv /tmp/lib-crash-bashrc-root "$bash_file_root" || exit 1
 }
 
 if [ "$arg_method" == "local" ]
@@ -108,6 +153,8 @@ then
     rm -r "$arg_path" || exit 1
     sed '/# lib-crash start/,/# lib-crash end/d' "$bash_file" > /tmp/lib-crash-bashrc || exit 1
     cp /tmp/lib-crash-bashrc "$bash_file" || exit 1
+    sed '/# lib-crash start/,/# lib-crash end/d' "$bash_file_root" > /tmp/lib-crash-bashrc-root || exit 1
+    cp /tmp/lib-crash-bashrc-root "$bash_file_root" || exit 1
     log "successfully uninstalled lib-crash"
 else
     log "invalid argument '$arg_method' visit help page"
